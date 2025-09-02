7cfedc5a88d9bc3d3e820e4873bd4fe6d8a015bc
vitalii_cherukha <vitalii.cherukha@gmail.com>
2025-09-02 17:35:56 +0300

    update

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -5,19 +5,35 @@ import {
   QueryClient,
 } from '@tanstack/react-query';
 import NoteDetailsClient from './NoteDetails.client';
-// import { Metadata } from 'next';
+import { Metadata } from 'next';
 
 interface NoteDetailsProps {
   params: Promise<{ id: string }>;
 }
 
-// export const generateMetadata = async ({
-//   params,
-// }: NoteDetailsProps): Promise<Metadata> => {
-//   const { id } = await params;
-//   const note = await fetchNoteById(id);
-//   return {};
-// };
+export const generateMetadata = async ({
+  params,
+}: NoteDetailsProps): Promise<Metadata> => {
+  const { id } = await params;
+  const note = await fetchNoteById(id);
+  return {
+    title: note.title,
+    description: note.content.slice(0, 15) + '..',
+    openGraph: {
+      title: note.title,
+      description: note.content.slice(0, 15) + '..',
+      url: `https://08-zustand-ten-indol.vercel.app/notes/${id}`,
+      images: [
+        {
+          url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+          width: 1200,
+          height: 630,
+          alt: note.title,
+        },
+      ],
+    },
+  };
+};
 
 const NoteDetailsPage = async ({ params }: NoteDetailsProps) => {
   const { id } = await params;
